cc0de40d5129a7ec937f7ba5bb5a5457e27cf612
Sandip Chitale <sandipchitale@gmail.com>
2024-11-27 15:28:34 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,12 @@
+# chrome.history
+
+This extension uses the `chrome.history` API to delete all history of a site.
+
+## Overview
+
+## Running this extension
+
+1. Clone this repository.
+2. Load this directory in Chrome as an [unpacked extension](https://developer.chrome.com/docs/extensions/mv3/getstarted/development-basics/#load-unpacked).
+3. Pin the extension to the taskbar to access the action button.
+4. Open the extension popup by clicking the action button and interact with the UI.
---
+++ clear_history_worker.js
@@ -0,0 +1,22 @@
+(async () => {
+    chrome.action.onClicked.addListener(async (tab) => {
+        const historyItems = await chrome.history.search({
+            text: '',
+            maxResults: 65536,
+            startTime: 0
+        });
+        historyItems.forEach((historyItem) => {
+            console.log(`Item ${historyItem.url}`);
+            if (historyItem.url.includes('.facebook.com/')
+                || historyItem.url.includes('.pinterest.com/')
+                || historyItem.url.includes('.pinimg.com/')) {
+                (async (historyItem) => {
+                    console.log(`Deleting ${historyItem.url} with id ${historyItem.id}`);
+                    await chrome.history.deleteUrl({
+                        url: historyItem.url
+                    })
+                })(historyItem);
+            }
+        });
+    });
+})();
